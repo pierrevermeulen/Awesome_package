fe0856b4e644f360b6e06399a72d1d89fd72626d
Pierre Vermeulen <pierre.vermeulen@espci.org>
2020-11-30 13:48:57 +0100

    Update package

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -3,17 +3,7 @@
 # Import from standard library
 import os
 import Awesome_package
-import pandas as pd
 # Import from our lib
-from Awesome_package.lib import clean_data
+from Awesome_package.lib import try_me
 import pytest
 
-
-def test_clean_data():
-    datapath = os.path.dirname(os.path.abspath(Awesome_package.__file__)) + '/data'
-    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
-    first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
-    assert list(df.columns)[:6] == first_cols
-    assert df.shape == (999, 142)
-    out = clean_data(df)
-    assert out.shape == (985, 119)
